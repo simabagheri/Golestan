62d33d0cba92086b75ca1d215a6bdda00af47366
simabagheri <sima.bagheri1@gmail.com>
2017-05-25 13:37:42 +0430

    Add files via upload
    login is ok

--- LoginController.java
+++ LoginController.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.sbu.controller;
 
-import com.sbu.controller.model.Student;
+package com.sbu.controller;
+import com.sbu.dao.model.User;
+import com.sbu.service.impl.UserManagerImpl;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,26 +16,53 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/login")       //welcome.jsp
 public class LoginController { 
-   
-    @RequestMapping(method = RequestMethod.GET)  //servlet without special url
-   public String addStudent(HttpServletRequest request,HttpServletResponse response) {      
+    
+    
+    
+     @Autowired
+    public UserManagerImpl userManagerImpl;
+     
+     
+    @RequestMapping(method = RequestMethod.GET)  //servlet without special url always running in welcome page
+   public String loggining(HttpServletRequest request,HttpServletResponse response) { 
+       
       String user= request.getParameter("username");
       String pass= request.getParameter("password");
+      boolean has_registered=false;
+      int index=-1;
       
-      if(user.equals("student") && pass.equals("student"))
+      List<User> list_users=userManagerImpl.findAllUsers();
+      
+      
+      for(int i=0;i<list_users.size();i++)
       {
-          return "student";
+          if(user.equals(list_users.get(i).getUsername()) && pass.equals(list_users.get(i).getPassword()))
+          {
+              has_registered=true;
+              index=i;
+              break;
+          }
       }
       
-      else if(user.equals("teacher") && pass.equals("teacher"))
+      if(has_registered)
       {
-          return "teacher"; 
+      
+            if( list_users.get(index).getType().equals("student"))
+                    
+            {
+                return "student";
+            }
+            else if( list_users.get(index).getType().equals("manager"))
+            {
+                return "manager";
+            }
+            else if( list_users.get(index).getType().equals("teacher"))
+            {
+                return "teacher";
+            } 
       }
-      else if(user.equals("manager") && pass.equals("manager"))
-      {
-          return "manager";
-      }
-      else
-          return "welcome";
-   }
+      
+      return "welcome";
 }
+    
+   }
